4b48d100aa048c1a23c4196ed6bc0f237ac05fdb
timoheller99 <timo.heller@5minds.de>
2020-11-16 19:33:56 +0100

    add method to register a http client to the ioc

--- src/FactroApiClient/ServiceCollectionExtensions.cs
+++ src/FactroApiClient/ServiceCollectionExtensions.cs
@@ -1,12 +1,38 @@
 namespace FactroApiClient
 {
+    using System;
+    using System.Net.Http.Headers;
+
+    using Microsoft.Extensions.Configuration;
     using Microsoft.Extensions.DependencyInjection;
 
     public static class ServiceCollectionExtensions
     {
-        public static void AddFactroApiClientServices(this IServiceCollection serviceCollection)
+        public static void AddFactroApiClientServices(this IServiceCollection serviceCollection, IConfigurationRoot configurationRoot)
         {
+            serviceCollection.RegisterHttpClient(configurationRoot);
+
             // serviceCollection.AddTransient<ITestService, TestService>();
+        }
+
+        private static void RegisterHttpClient(this IServiceCollection serviceCollection, IConfigurationRoot configurationRoot)
+        {
+            var configSection = configurationRoot.GetSection("FactroApiClient").GetSection("ApiToken");
+            var apiToken = configSection.Value;
+            if (apiToken == null)
+            {
+                throw new Exception($"Could not find Factro API token in '{configSection.Path}'.");
+            }
+
+            serviceCollection.AddHttpClient(
+                "BaseClient",
+                (provider, client) =>
+            {
+                client.DefaultRequestHeaders.Accept.Clear();
+                client.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
+                client.DefaultRequestHeaders.Add("authorization", apiToken);
+                client.BaseAddress = new Uri("https://cloud.factro.com/api/core");
+            });
         }
     }
 }
