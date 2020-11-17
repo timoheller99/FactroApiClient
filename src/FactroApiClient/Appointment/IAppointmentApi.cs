b0afaccd28f2f2079605a35304ea1c25b838b9f7
timoheller99 <timo.heller@5minds.de>
2020-11-17 18:12:59 +0100

    add xml documentation comments to the interface's members

--- src/FactroApiClient/Appointment/IAppointmentApi.cs
+++ src/FactroApiClient/Appointment/IAppointmentApi.cs
@@ -5,16 +5,47 @@ namespace FactroApiClient.Appointment
 
     using FactroApiClient.Appointment.Contracts;
 
+    /// <summary>
+    /// Interface for the Appointment API
+    /// </summary>
+    /// <remarks>
+    /// Appointments are not yet booked Work-Records.
+    /// </remarks>
     public interface IAppointmentApi
     {
+        /// <summary>
+        /// Fetches the appointments visible to the requesting user.
+        /// </summary>
+        /// <returns>Returns a list of all appointments that are visible to the requesting user.</returns>
         public Task<IEnumerable<GetAppointmentPayload>> GetAppointmentsAsync();
 
+        /// <summary>
+        /// Creates an appointment.
+        /// </summary>
+        /// <param name="createAppointmentRequest">The request model to create a new appointment.</param>
+        /// <returns>Returns the created appointment.</returns>
         public Task<CreateAppointmentResponse> CreateAppointmentAsync(CreateAppointmentRequest createAppointmentRequest);
 
+        /// <summary>
+        /// Fetches the appointment with the given appointment id.
+        /// </summary>
+        /// <param name="appointmentId">The id of the appointment that should be fetched.</param>
+        /// <returns>Returns the fetched appointment.</returns>
         public Task<GetAppointmentByIdResponse> GetAppointmentAsync(string appointmentId);
 
+        /// <summary>
+        /// Updates the appointment with the given appointment id.
+        /// </summary>
+        /// <param name="appointmentId">The id oof the appointment that should be updated.</param>
+        /// <param name="updateAppointmentRequest">The request model to update the appointment.</param>
+        /// <returns>Returns the updated appointment</returns>
         public Task<UpdateAppointmentResponse> UpdateAppointmentAsync(string appointmentId, UpdateAppointmentRequest updateAppointmentRequest);
 
+        /// <summary>
+        /// Deletes the appointment with the given appointment id.
+        /// </summary>
+        /// <param name="appointmentId">The id of the appointment that should be deleted.</param>
+        /// <returns>Returns the deleted appointment.</returns>
         public Task<DeleteAppointmentResponse> DeleteAppointmentAsync(string appointmentId);
     }
 }
