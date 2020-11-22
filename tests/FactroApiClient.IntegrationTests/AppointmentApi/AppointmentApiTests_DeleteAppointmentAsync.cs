552ea252cc26277dd4864a787f584a680df8bae5
timoheller99 <timo.heller@5minds.de>
2020-11-22 20:41:05 +0100

    add integration tests

--- tests/FactroApiClient.IntegrationTests/AppointmentApi/AppointmentApiTests_DeleteAppointmentAsync.cs
+++ tests/FactroApiClient.IntegrationTests/AppointmentApi/AppointmentApiTests_DeleteAppointmentAsync.cs
@@ -1,6 +1,57 @@
 namespace FactroApiClient.IntegrationTests.AppointmentApi
 {
+    using System;
+    using System.Threading.Tasks;
+
+    using FactroApiClient.Appointment;
+    using FactroApiClient.Appointment.Contracts;
+
+    using FluentAssertions;
+
+    using Xunit;
+
     public partial class AppointmentApiTests
     {
+        [Fact]
+        public async Task DeleteAppointmentAsync_ExistingAppointment_ShouldDeleteAppointment()
+        {
+            // Arrange
+            var appointmentApi = this.fixture.GetService<IAppointmentApi>();
+
+            const string employeeId = BaseTestFixture.ValidEmployeeId;
+            var startDate = DateTime.Now;
+            var endDate = startDate.AddHours(1);
+            var subject = $"{BaseTestFixture.TestPrefix}{Guid.NewGuid().ToString()}";
+
+            var createAppointmentRequest = new CreateAppointmentRequest(employeeId, startDate, endDate, subject);
+            var existingAppointment = await appointmentApi.CreateAppointmentAsync(createAppointmentRequest);
+
+            var deletedAppointment = new DeleteAppointmentResponse();
+
+            // Act
+            Func<Task> act = async () => deletedAppointment = await appointmentApi.DeleteAppointmentAsync(existingAppointment.Id);
+
+            // Assert
+            await act.Should().NotThrowAsync();
+
+            deletedAppointment.Should().BeEquivalentTo(existingAppointment);
+        }
+
+        [Fact]
+        public async Task DeleteAppointmentAsync_NotExistingAppointment_ShouldDeleteAppointment()
+        {
+            // Arrange
+            var appointmentApi = this.fixture.GetService<IAppointmentApi>();
+
+            var deletedAppointment = new DeleteAppointmentResponse();
+
+            // Act
+            Func<Task> act = async () => deletedAppointment = await appointmentApi.DeleteAppointmentAsync(Guid.NewGuid().ToString());
+
+            // Assert
+            await act.Should().NotThrowAsync();
+
+            deletedAppointment.Should().BeNull();
+        }
     }
 }
