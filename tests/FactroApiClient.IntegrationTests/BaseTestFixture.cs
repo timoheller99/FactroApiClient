4ef37a40d42ed8e7e14c00371a8e72bacfcfb3c0
timoheller99 <timo.heller@5minds.de>
2020-11-22 15:46:53 +0100

    add a base fixturre to setup FactroApiClient

---
+++ tests/FactroApiClient.IntegrationTests/BaseTestFixture.cs
@@ -0,0 +1,31 @@
+namespace FactroApiClient.IntegrationTests
+{
+    using System;
+    using System.Threading.Tasks;
+
+    using FactroApiClient.IntegrationTests.Setup;
+
+    using Microsoft.Extensions.DependencyInjection;
+
+    public class BaseTestFixture
+    {
+        public const string ApiUserId = "5d82d596-308e-4df1-b199-9b62ae7458f2";
+
+        private readonly IServiceProvider serviceProvider;
+
+        public BaseTestFixture()
+        {
+            this.serviceProvider = Startup.ConfigureServices(new ServiceCollection()).BuildServiceProvider();
+        }
+
+        public T GetService<T>()
+        {
+            return this.serviceProvider.GetRequiredService<T>();
+        }
+
+        public virtual async Task ClearFactroInstanceAsync()
+        {
+            await Task.CompletedTask;
+        }
+    }
+}
