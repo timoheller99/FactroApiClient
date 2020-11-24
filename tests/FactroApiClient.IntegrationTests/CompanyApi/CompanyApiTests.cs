423ddcfcd98f0db90a54b0e10d4c536a93b29633
timoheller99 <timo.heller@5minds.de>
2020-11-24 18:55:07 +0100

    add partial class for tests

---
+++ tests/FactroApiClient.IntegrationTests/CompanyApi/CompanyApiTests.cs
@@ -0,0 +1,18 @@
+namespace FactroApiClient.IntegrationTests.CompanyApi
+{
+    using System.Diagnostics.CodeAnalysis;
+
+    using Xunit;
+
+    [SuppressMessage("Naming Rules", "MA0048", Justification = "Improve readability with partial class.")]
+    [SuppressMessage("Naming Rules", "VSTHRD200", Justification = "Improve readability with partial class.")]
+    public partial class CompanyApiTests : IClassFixture<CompanyApiTestFixture>
+    {
+        private readonly CompanyApiTestFixture fixture;
+
+        public CompanyApiTests(CompanyApiTestFixture fixture)
+        {
+            this.fixture = fixture;
+        }
+    }
+}
