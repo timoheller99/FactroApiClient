7110e800a37de5091c5917e2163a820e41f51072
timoheller99 <timo.heller@5minds.de>
2020-12-14 17:23:03 +0100

    add integration tests

---
+++ tests/FactroApiClient.IntegrationTests/CompanyApi/CompanyApiTests_CreateCompanyTagAsync.cs
@@ -0,0 +1,71 @@
+namespace FactroApiClient.IntegrationTests.CompanyApi
+{
+    using System;
+    using System.Threading.Tasks;
+
+    using FactroApiClient.Company;
+    using FactroApiClient.Company.Contracts.CompanyTag;
+
+    using FluentAssertions;
+    using FluentAssertions.Execution;
+
+    using Xunit;
+
+    public partial class CompanyApiTests
+    {
+        [Fact]
+        public async Task CreateCompanyTagAsync_ValidCompanyTag_ShouldCreateCompanyTag()
+        {
+            // Arrange
+            var companyApi = this.fixture.GetService<ICompanyApi>();
+
+            var name = $"{BaseTestFixture.TestPrefix}{Guid.NewGuid().ToString()}";
+
+            var createCompanyTagRequest = new CreateCompanyTagRequest(name);
+
+            var createCompanyTagResponse = default(CreateCompanyTagResponse);
+
+            // Act
+            Func<Task> act = async () => createCompanyTagResponse = await companyApi.CreateCompanyTagAsync(createCompanyTagRequest);
+
+            // Assert
+            await act.Should().NotThrowAsync();
+
+            var companyTags = await this.fixture.GetCompanyTagsAsync(companyApi);
+
+            companyTags.Should().ContainEquivalentOf(createCompanyTagResponse);
+        }
+
+        [Fact]
+        public async Task CreateCompanyTagAsync_TwoCompanyTagsWithSameName_ShouldStoreBothCompanyTags()
+        {
+            // Arrange
+            var companyApi = this.fixture.GetService<ICompanyApi>();
+
+            var name = $"{BaseTestFixture.TestPrefix}{Guid.NewGuid().ToString()}";
+
+            var createCompanyTagRequest = new CreateCompanyTagRequest(name);
+
+            var firstCreatedCompanyTag = await companyApi.CreateCompanyTagAsync(createCompanyTagRequest);
+
+            var secondCreatedCompanyTag = default(CreateCompanyTagResponse);
+
+            // Act
+            Func<Task> act = async () => secondCreatedCompanyTag = await companyApi.CreateCompanyTagAsync(createCompanyTagRequest);
+
+            // Assert
+            await act.Should().NotThrowAsync();
+
+            using (new AssertionScope())
+            {
+                secondCreatedCompanyTag.Should().NotBeNull();
+
+                var companyTags = await this.fixture.GetCompanyTagsAsync(companyApi);
+
+                companyTags.Should()
+                    .Contain(x => x.Id == firstCreatedCompanyTag.Id).And
+                    .Contain(x => x.Id == secondCreatedCompanyTag.Id);
+            }
+        }
+    }
+}
---
+++ tests/FactroApiClient.IntegrationTests/CompanyApi/CompanyApiTests_RemoveCompanyTagAsync.cs
@@ -0,0 +1,80 @@
+namespace FactroApiClient.IntegrationTests.CompanyApi
+{
+    using System;
+    using System.Threading.Tasks;
+
+    using FactroApiClient.Company;
+    using FactroApiClient.Company.Contracts.CompanyTag;
+
+    using FluentAssertions;
+    using FluentAssertions.Execution;
+
+    using Xunit;
+
+    public partial class CompanyApiTests
+    {
+        [Fact]
+        public async Task RemoveCompanyTagAsync_ExistingCompanyWithAssociation_ShouldRemoveCompanyTag()
+        {
+            // Arrange
+            var companyApi = this.fixture.GetService<ICompanyApi>();
+
+            var existingCompany = await this.fixture.CreateTestCompanyAsync(companyApi);
+
+            var createdCompanyTag = await this.fixture.CreateTestCompanyTagAsync(companyApi);
+
+            var addCompanyTagRequest = new AddCompanyTagAssociationRequest(createdCompanyTag.Id);
+
+            await companyApi.AddCompanyTagAsync(existingCompany.Id, addCompanyTagRequest);
+
+            // Act
+            Func<Task> act = async () => await companyApi.RemoveCompanyTagAsync(existingCompany.Id, createdCompanyTag.Id);
+
+            // Assert
+            await act.Should().NotThrowAsync();
+
+            using (new AssertionScope())
+            {
+                (await companyApi.GetCompanyTagsByIdAsync(existingCompany.Id)).Should().NotContain(x => x.Id == createdCompanyTag.Id);
+
+                (await this.fixture.GetCompanyTagsAsync(companyApi)).Should().ContainEquivalentOf(createdCompanyTag);
+            }
+        }
+
+        [Fact]
+        public async Task RemoveCompanyTagAsync_ExistingCompanyWithoutAssociation_ShouldNotThrow()
+        {
+            // Arrange
+            var companyApi = this.fixture.GetService<ICompanyApi>();
+
+            var existingCompany = await this.fixture.CreateTestCompanyAsync(companyApi);
+
+            var createdCompanyTag = await this.fixture.CreateTestCompanyTagAsync(companyApi);
+
+            // Act
+            Func<Task> act = async () => await companyApi.RemoveCompanyTagAsync(existingCompany.Id, createdCompanyTag.Id);
+
+            // Assert
+            await act.Should().NotThrowAsync();
+
+            (await companyApi.GetCompanyTagsByIdAsync(existingCompany.Id)).Should().NotContain(x => x.Id == createdCompanyTag.Id);
+        }
+
+        [Fact]
+        public async Task RemoveCompanyTagAsync_NotExistingCompany_ShouldNotThrow()
+        {
+            // Arrange
+            var companyApi = this.fixture.GetService<ICompanyApi>();
+
+            var createdCompanyTag = await this.fixture.CreateTestCompanyTagAsync(companyApi);
+
+            var companyId = Guid.NewGuid().ToString();
+
+            // Act
+            Func<Task> act = async () => await companyApi.RemoveCompanyTagAsync(companyId, createdCompanyTag.Id);
+
+            // Assert
+            await act.Should().NotThrowAsync();
+        }
+    }
+}
