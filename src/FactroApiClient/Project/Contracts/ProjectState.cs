2c215c053321cb48b3670c63d6f4763f368c952f
timoheller99 <timo.heller@5minds.de>
2020-12-17 18:12:18 +0100

    add project state contract

---
+++ src/FactroApiClient/Project/Contracts/ProjectState.cs
@@ -0,0 +1,11 @@
+namespace FactroApiClient.Project.Contracts
+{
+    public enum ProjectState
+    {
+        Planned,
+        InProgress,
+        Closed,
+        PushedBack,
+        Stopped,
+    }
+}
