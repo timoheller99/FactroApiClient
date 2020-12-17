c9ead391d8448c272eb0a2391a40afd510f90bba
timoheller99 <timo.heller@5minds.de>
2020-12-17 18:11:28 +0100

    add document contracts

---
+++ src/FactroApiClient/Project/Contracts/Document/IGetProjectDocumentPayload.cs
@@ -0,0 +1,27 @@
+namespace FactroApiClient.Project.Contracts.Document
+{
+    using System;
+
+    using FactroApiClient.SharedContracts;
+
+    public interface IGetProjectDocumentPayload
+    {
+        public DateTime ChangeDate { get; set; }
+
+        public DateTime CreationDate { get; set; }
+
+        public string CreatorId { get; set; }
+
+        public string Id { get; set; }
+
+        public string ContentType { get; set; }
+
+        public string ReferenceId { get; set; }
+
+        public DocumentReferenceType ReferenceType { get; set; }
+
+        public double Size { get; set; }
+
+        public string Title { get; set; }
+    }
+}
