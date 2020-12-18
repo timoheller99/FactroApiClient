e8425871c3be6a6cc703a85962609784288c936c
timoheller99 <timo.heller@5minds.de>
2020-12-18 15:34:22 +0100

    fix project structure tree contract property type

--- src/FactroApiClient/Project/Contracts/Structure/ProjectStructureTree.cs
+++ src/FactroApiClient/Project/Contracts/Structure/ProjectStructureTree.cs
@@ -1,11 +1,13 @@
 namespace FactroApiClient.Project.Contracts.Structure
 {
+    using System.Collections.Generic;
+
     public class ProjectStructureTree
     {
         public string Id { get; set; }
 
         public StructureType Type { get; set; }
 
-        public ProjectStructureTree Children { get; set; }
+        public IEnumerable<ProjectStructureTree> Children { get; set; }
     }
 }
