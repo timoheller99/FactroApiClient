0ad2ed4bb23c46ffeb413c2621d70053051996d2
timoheller99 <timo.heller@5minds.de>
2020-12-18 22:30:27 +0100

    add needed fixture property

--- tests/FactroApiClient.UnitTests/ProjectApi/ProjectApiTestFixture.cs
+++ tests/FactroApiClient.UnitTests/ProjectApi/ProjectApiTestFixture.cs
@@ -22,6 +22,13 @@ namespace FactroApiClient.UnitTests.ProjectApi
             new object[] { " " },
         };
 
+        public static readonly IEnumerable<object[]> InvalidProjectCommentIds = new List<object[]>
+        {
+            new object[] { null },
+            new object[] { string.Empty },
+            new object[] { " " },
+        };
+
         public ProjectApi GetProjectApi(HttpResponseMessage response = null)
         {
             var loggerMock = this.GetLoggerMock();
