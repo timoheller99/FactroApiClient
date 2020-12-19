2985a2508ecdd3c1b2bc67997ea4237bc4e4a7e4
timoheller99 <timo.heller@5minds.de>
2020-12-19 18:51:33 +0100

    make ApiEndpoints not partial

--- src/FactroApiClient/Endpoints/ApiEndpoints.cs
+++ src/FactroApiClient/Endpoints/ApiEndpoints.cs
@@ -1,10 +1,6 @@
 namespace FactroApiClient.Endpoints
 {
-    using System.Diagnostics.CodeAnalysis;
-
-    [SuppressMessage("Naming Rules", "MA0048", Justification = "Improve readability with partial class.")]
-    [SuppressMessage("Naming Rules", "VSTHRD200", Justification = "Improve readability with partial class.")]
-    internal static partial class ApiEndpoints
+    internal static class ApiEndpoints
     {
         public static string BaseAddress = "https://cloud.factro.com/api/core/";
     }
