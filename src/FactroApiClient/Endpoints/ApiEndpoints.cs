9540415410e423fa7b63447fa159c44eabb1e2ae
timoheller99 <timo.heller@5minds.de>
2020-12-19 18:51:45 +0100

    make BaseAddress const

--- src/FactroApiClient/Endpoints/ApiEndpoints.cs
+++ src/FactroApiClient/Endpoints/ApiEndpoints.cs
@@ -2,6 +2,6 @@ namespace FactroApiClient.Endpoints
 {
     internal static class ApiEndpoints
     {
-        public static string BaseAddress = "https://cloud.factro.com/api/core/";
+        public const string BaseAddress = "https://cloud.factro.com/api/core/";
     }
 }
