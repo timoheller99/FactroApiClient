c7d4bafcf74d4813ac14a004a5178c68a5e6db1a
timoheller99 <timo.heller@5minds.de>
2020-12-25 21:31:00 +0100

    add tests for CreateProject

---
+++ tests/FactroApiClient.IntegrationTests/ProjectApi/ProjectApiTests_CreateProjectAsync.cs
@@ -0,0 +1,69 @@
+namespace FactroApiClient.IntegrationTests.ProjectApi
+{
+    using System;
+    using System.Linq;
+    using System.Threading.Tasks;
+
+    using FactroApiClient.Project;
+    using FactroApiClient.Project.Contracts.Base;
+
+    using FluentAssertions;
+    using FluentAssertions.Execution;
+
+    using Xunit;
+
+    public partial class ProjectApiTests
+    {
+        [Fact]
+        public async Task CreateProjectAsync_ValidProject_ShouldCreateProject()
+        {
+            // Arrange
+            var projectApi = this.fixture.GetService<IProjectApi>();
+
+            var title = $"{BaseTestFixture.TestPrefix}{Guid.NewGuid().ToString()}";
+
+            var createProjectRequest = new CreateProjectRequest(title);
+
+            var createProjectResponse = default(CreateProjectResponse);
+
+            // Act
+            Func<Task> act = async () => createProjectResponse = await projectApi.CreateProjectAsync(createProjectRequest);
+
+            // Assert
+            await act.Should().NotThrowAsync();
+
+            using (new AssertionScope())
+            {
+                var projects = await this.fixture.GetProjectsAsync(projectApi);
+
+                projects.Should().ContainEquivalentOf(createProjectResponse);
+            }
+        }
+
+        [Fact]
+        public async Task CreateProjectAsync_TwoProjectsWithSameTitle_ShouldCreateProjectTwice()
+        {
+            // Arrange
+            var projectApi = this.fixture.GetService<IProjectApi>();
+
+            var title = $"{BaseTestFixture.TestPrefix}{Guid.NewGuid().ToString()}";
+
+            var createProjectRequest = new CreateProjectRequest(title);
+
+            await projectApi.CreateProjectAsync(createProjectRequest);
+
+            // Act
+            Func<Task> act = async () => await projectApi.CreateProjectAsync(createProjectRequest);
+
+            // Assert
+            await act.Should().NotThrowAsync();
+
+            using (new AssertionScope())
+            {
+                var projects = await this.fixture.GetProjectsAsync(projectApi);
+
+                projects.Where(project => project.Title == title).Should().HaveCount(2);
+            }
+        }
+    }
+}
