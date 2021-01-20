33616d5dbab95bd890efb248e0334df750bba29f
timoheller99 <timo.heller@5minds.de>
2021-01-20 16:42:01 +0100

    add EnumMember attributes to enums to define value

--- src/FactroApiClient/Project/Contracts/ProjectState.cs
+++ src/FactroApiClient/Project/Contracts/ProjectState.cs
@@ -1,11 +1,24 @@
 namespace FactroApiClient.Project.Contracts
 {
+    using System.Runtime.Serialization;
+
     public enum ProjectState
     {
+        NotDefined = 0,
+
+        [EnumMember(Value = "planned")]
         Planned,
+
+        [EnumMember(Value = "inProcess")]
         InProgress,
+
+        [EnumMember(Value = "closed")]
         Closed,
+
+        [EnumMember(Value = "pushedBack")]
         PushedBack,
+
+        [EnumMember(Value = "stopped")]
         Stopped,
     }
 }
